8780ab8760258123ebd4702990405493ccb87be9
Saarthi09 <saarthibasantgupta31@gmail.com>
2025-10-06 20:23:33 +0530

    Few minor changes for more efficient code

--- Student Attendance Program/Attendance_Marking.py
+++ Student Attendance Program/Attendance_Marking.py
@@ -1,7 +1,8 @@
-def Mark_Present(student_id):
+def Mark_Present(student_id, name):
     try:
         with open("student_ids.txt", "r") as file:
             student_ids = [line.strip() for line in file.readlines()]
+            student_names = [line.strip() for line in file.readlines() [1:]]
     
     except FileNotFoundError:
         print("Error: No student IDs found. Please generate IDs first.")
@@ -13,7 +14,7 @@ def Mark_Present(student_id):
     
     with open("attendance.txt", "a") as file: 
         file.write(f"{student_id} - Present\n")
-    print(f"Attendance marked for student ID: {student_id}")
+    print(f"Attendance marked for student ID: {student_names}, {student_id}")
 
 if __name__ == "__main__":
     student_id = input("Enter Student ID: ").strip()
--- Student Attendance Program/ID_Generator.py
+++ Student Attendance Program/ID_Generator.py
@@ -3,14 +3,16 @@ import random
 def generate_studentid():
     return random.randint(100000,999999)
 
-name = input("Please enter your first name: ").strip()
 
 def save_student_id(student_id):
     with open("student_ids.txt", "a") as file:
         file.write(f"{student_id}, {name} \n")
 
 if __name__ == "__main__":
+    name = input("Please enter your first name: ").strip()  
+
     student_id = generate_studentid()
-    print(f"Generated Student ID: {student_id}")
-    save_student_id(student_id)
+
+    print(f"Generated Student ID for {name}: {student_id}")
+    save_student_id(student_id, name)
     print("Student ID saved successfully")
