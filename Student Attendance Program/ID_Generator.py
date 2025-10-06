dcc5c9ff9fcc332898e17179cc82469a28be1aef
Saarthi09 <saarthibasantgupta31@gmail.com>
2025-10-06 20:16:00 +0530

    Adding names to student IDs

--- Student Attendance Program/ID_Generator.py
+++ Student Attendance Program/ID_Generator.py
@@ -3,9 +3,11 @@ import random
 def generate_studentid():
     return random.randint(100000,999999)
 
+name = input("Please enter your first name: ").strip()
+
 def save_student_id(student_id):
     with open("student_ids.txt", "a") as file:
-        file.write(str(student_id)+ "\n")
+        file.write(f"{student_id}, {name} + \n")
 
 if __name__ == "__main__":
     student_id = generate_studentid()
