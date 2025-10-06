6d7f51c536aece415f426bcddc17856d379b69aa
Saarthi09 <saarthibasantgupta31@gmail.com>
2025-10-06 20:16:40 +0530

    Making appropriate chanegs

--- Student Attendance Program/ID_Generator.py
+++ Student Attendance Program/ID_Generator.py
@@ -7,7 +7,7 @@ name = input("Please enter your first name: ").strip()
 
 def save_student_id(student_id):
     with open("student_ids.txt", "a") as file:
-        file.write(f"{student_id}, {name} + \n")
+        file.write(f"{student_id}, {name} \n")
 
 if __name__ == "__main__":
     student_id = generate_studentid()
