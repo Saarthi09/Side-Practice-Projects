25b2c9eee97120303cc9a095ac62bc868bc6dd18
Saarthi09 <saarthibasantgupta31@gmail.com>
2025-10-12 21:10:59 +0530

    initial code, checking loop

---
+++ Experimenting Variations of PENNY'S GAME/code.py
@@ -0,0 +1,13 @@
+import random
+outcomes = list()
+outcomes.append('heads')
+outcomes.append('tails')
+count = 0
+#a = int(input("enter a 3 toss sequence (eg: HTT)"))
+series = list()
+b = int(input("how many tosses: "))
+for i in series:
+    count =+ 1
+    y = random.choice(outcomes, k = 1)
+    series.append(y)
+print(series)
